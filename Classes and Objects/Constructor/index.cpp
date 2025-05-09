857d660b2432c32fee69491a1c25152b7d5e783b
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-05-09 13:04:47 +0530

    constructor

---
+++ Classes and Objects/Constructor/index.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+    private:
+        int a, b;
+    public:
+        // constructor of the class
+        Complex(){
+            a=0;
+            b=0;
+            // cout<<"Counstructor called"<<endl;
+        }
+
+        // parameterzed constructor
+        // Complex(int p, int q){
+        //     a=p;
+        //     b=q;
+        // };
+
+        void setData(int x, int y){
+            a=x;
+            b=y;
+        };
+
+        void showData(){
+            cout<<"\n"<<a<<"\n"<<b;
+            cout<<endl;
+        }
+};
+
+int main(){
+
+    Complex c1;
+
+    c1.setData(1,2);
+    c1.showData();
+
+    return 0;
+}
