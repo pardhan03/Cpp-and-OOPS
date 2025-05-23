c8c0a91e97c27c25d7b1665755fcd6f18f62e380
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-05-23 13:05:05 +0530

    virtual function

--- Classes and Objects/Inheritance/inheritance.cpp
+++ Classes and Objects/Inheritance/inheritance.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 class A{
     public:
-        void f1(int a){
+        // using virtual now p->f1 call the method of B class
+        virtual void f1(int a){
             cout<<"A-f1()"<<endl;
         }
 };
@@ -14,10 +15,20 @@ class B: public A{
         void f1(int a){
             cout<<"B-f1()"<<endl;
         }
+        void f2(){
+            cout<<"f2"<<endl;
+        }
 };
 
 int main() {
-    B obj;
-    obj.f1(1);
+    // B obj;
+    // obj.f1(1);
+
+    A *p; // parnet pointer
+    p= new B; // desending class object
+    // this will run the method of A cleass insted of B class
+    p->f1(5);
+    // p->f2() error
+
     return 0;
 }
